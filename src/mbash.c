84bf7742b7b3c43dad4fea32708bcfbd80fb7afe
root <root@HugoC-Inspiron>
2023-01-16 23:27:27 +0100

    structuring code into sub-functions

--- src/mbash.c
+++ src/mbash.c
@@ -3,20 +3,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
-#define MAXLI 2048
+#include <sys/wait.h>
+#include <errno.h>
 
+#define MAXLI 2048
 
 char cmd[MAXLI];
 char path[MAXLI];
 int pathidx;
 
-//Pre-declaration of functions
+//Functions pre-declarations
 void mbash();
 void run();
 void welcome();
-void input();
-void output();
+void capture();
+void processing();
 void checkExit();
+void interpreter();
+void identifyTokens(char *arg_list[], char *p, int count);
+void execute();
 
 
 int main(int argc, char** argv) {
@@ -30,8 +35,8 @@ void run()
     welcome();
 
     while (1) {
-        input();
-        output(cmd);
+        capture();
+        processing();
     }
 }
 
@@ -44,7 +49,7 @@ void welcome()
     printf("-----------------\n");
 }
 
-void input()
+void capture()
 {
     printf("mbash> ");
     fgets(cmd, MAXLI, stdin);
@@ -52,15 +57,72 @@ void input()
 }
 
 
-void output() {
+void processing() {
     checkExit();
     printf("<< %s\n", cmd);
-    system(cmd);
+    //system(cmd);
+    interpreter();
 
 }
 
 void checkExit() {
     if (strcmp(cmd, "exit") == 0) {
         exit(EXIT_SUCCESS);
+    }
+}
+
+void interpreter()
+{
+    //Analyze the command
+    int count = 0; //number of arguments
+    int loop;
+    for (loop = 0 ; loop < strlen(cmd) ; ++ loop)
+    {
+        if (cmd[loop] == ' ') ++ count; //counting the number of spaces
+    }
+
+    char *arg_list[count+2]; //array of arguments
+    char *p = strdup(cmd); //duplicates cmd string
+
+    identifyTokens(arg_list, p, count);
+
+    execute(arg_list);
+
+    int increment=0;
+    while (arg_list[increment] != NULL)
+    {
+        free(arg_list[increment]);
+        increment ++;
+    }
+    free(p);
+}
+
+
+void identifyTokens(char *arg_list[], char *p, int count)
+{
+    char *tmp = strtok(p," "); //splits the duplicated string using spaces
+    int increment = 0; //index used to browse the arg_list array
+
+    while (tmp != NULL)
+    {
+        arg_list[increment] = strdup(tmp);
+        increment ++;
+        tmp = strtok(NULL," ");
+    }
+    arg_list[increment] = NULL;
+}
+
+void execute(char *arg_list[])
+{
+    pid_t process = fork();
+    if (process == 0) //parent process
+    {
+        int res = execvp(arg_list[0],arg_list);
+        if (res == -1) printf("%s\n",strerror(errno));
+        exit(0);
+    }
+    else //child process
+    {
+        wait(&process);
     }
 }
