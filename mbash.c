710904cbe9abd30345c25c2e1eeaf0616a23994e
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-01-16 16:04:03 +0100

    searches

--- mbash.c
+++ mbash.c
@@ -4,20 +4,137 @@
 #include <unistd.h>
 #include <assert.h>
 #define MAXLI 2048
+
+
 char cmd[MAXLI];
 char path[MAXLI];
+
 int pathidx;
+
 void mbash();
+void run();
+
+void lsh_loop(void);
+char *lsh_read_line(void)
+char **lsh_split_line(char *line)
+//void lsh_execute();
+int lsh_launch(char **args);
+
+
 int main(int argc, char** argv) {
-  while (1) {
-    printf("Commande: ");
-    fgets(cmd, MAXLI, stdin);
-    mbash(cmd);
-  }
+  //run();
+  lsh_loop();
   return 0;
 }
 
+void run()
+{
+    while (1) {
+        printf("mbash>> ");
+        fgets(cmd, MAXLI, stdin);
+        mbash(cmd);
+    }
+}
+
 void mbash() {
-  printf("Execute: %s", cmd);
+  printf("<< %s", cmd);
   system(cmd);
 }
+
+
+//-----tests lsh-----
+
+/*
+
+void lsh_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
+char *lsh_read_line(void)
+{
+  char *line = NULL;
+  ssize_t bufsize = 0; // have getline allocate a buffer for us
+
+  if (getline(&line, &bufsize, stdin) == -1){
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // We recieved an EOF
+    } else  {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return line;
+}
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+int lsh_launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    // Parent process
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+*/
