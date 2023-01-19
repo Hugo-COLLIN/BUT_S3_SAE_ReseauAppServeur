dd59d70bac3cc2593cf8625e5c47dd9429f7457b
root <root@HugoC-Inspiron>
2023-01-19 21:19:16 +0100

    background running (&) working but keep it active to the next commands

--- src/mbash.c
+++ src/mbash.c
@@ -7,13 +7,20 @@
 #include <errno.h>
 #include <glob.h>
 
+/*-----------------------------
+    Global Variables
+-----------------------------*/
 #define MAXLI 2048
 
 char cmd[MAXLI];
 char path[MAXLI];
 int pathidx;
+int nbArgs;
+int nbTokens;
 
-//Functions pre-declarations
+/*----------------------------------------
+    Functions pre-declarations
+----------------------------------------*/
 void mbash();
 void run();
 void welcome();
@@ -21,18 +28,19 @@ void capture();
 void prompt();
 void checkExit();
 void interpreter();
-void identifyTokens(char *arg_list[], char *c, int count);
+void identifyTokens(char *arg_list[], char *c);
 void executeCmd();
-int nbArguments();
+void nbArguments();
 void freeTokens(char *arg_list[], char *c);
 void execute(char *arg_list[]);
 int cdF(char *args[]);
 int helpF(char *args[]);
 int exitF(char *args[]);
+int inbuiltLength();
 
-/*
-  List of builtin commands
- */
+/*----------------------------------------
+        Inbuilt commands list
+----------------------------------------*/
 char *inbuiltStr[] = {
   "cd",
   "help",
@@ -45,13 +53,10 @@ int (*inbuiltF[]) (char **) = {
   &exitF
 };
 
-int inbuiltLength() {
-  return sizeof(inbuiltStr) / sizeof(char *);
-}
 
-/**
- * Main function
- */
+/** ----------------------------------------
+        Main functions
+---------------------------------------- **/
 int main(int argc, char** argv) {
     run();
     return 0;
@@ -68,6 +73,9 @@ void run()
 }
 
 
+/** ----------------------------------------
+        Welcome message
+---------------------------------------- **/
 void welcome()
 {
     printf("\n-----------------\n");
@@ -75,6 +83,80 @@ void welcome()
     printf("Hugo COLLIN - 16/01/2023\n\n");
     printf("Type 'exit' to exit mBash.\n");
     printf("-----------------\n");
+}
+
+/** ----------------------------------------
+        Command processing
+---------------------------------------- **/
+void interpreter()
+{
+    capture();
+
+    nbArguments();
+    char *arg_list[nbArgs + 2]; //array of arguments
+    char *c = strdup(cmd); //duplicates cmd string
+
+    identifyTokens(arg_list, c);
+    // Si & alors fork execute et waitpid sinon rien
+    execute(arg_list);
+    freeTokens(arg_list, c);
+}
+
+/** ----------------------------------------
+        Capture user input
+---------------------------------------- **/
+void capture()
+{
+    printf("\nmBash-%i> ", getpid());
+    fgets(cmd, MAXLI, stdin);
+    cmd[strlen(cmd) - 1] = '\0';
+}
+
+/** ----------------------------------------
+        Return the number of arguments
+---------------------------------------- **/
+void nbArguments()
+{
+    nbArgs = 0; //number of arguments
+    for (int loop = 0 ; loop < strlen(cmd) ; ++ loop)
+    {
+        if (cmd[loop] == ' ') ++ nbArgs; //counting the number of spaces
+    }
+}
+
+/** ----------------------------------------
+    Analyse the command and split it
+---------------------------------------- **/
+void identifyTokens(char *arg_list[], char *c)
+{
+    char *tmp = strtok(c, " "); //splits the duplicated string using spaces
+    int index = 0; //index used to browse the arg_list array
+
+    while (tmp != NULL)
+    {
+        char *c = strstr(tmp, "*");
+        if (c == NULL)
+        {
+            arg_list[index] = strdup(tmp);
+            index ++;
+        }
+        else
+        {
+            glob_t g;
+            int resGlob = glob(tmp, 0, NULL, &g);
+            if (resGlob == 0)
+            {
+                for (int loop = 0 ; loop < g.gl_pathc ; ++ loop)
+                {
+                    arg_list[index] = strdup(g.gl_pathv[loop]);
+                    index ++;
+                }
+            }
+            globfree(&g);
+        }
+        tmp = strtok(NULL, " ");
+    }
+    arg_list[index] = NULL;
 }
 
 void execute(char *arg_list[])
@@ -93,100 +175,52 @@ void execute(char *arg_list[])
   executeCmd(arg_list);
 }
 
-//-----------------
-
-void interpreter()
-{
-    capture();
-
-    int count = nbArguments();
-    char *arg_list[count+2]; //array of arguments
-    char *c = strdup(cmd); //duplicates cmd string
-
-    identifyTokens(arg_list, c, count);
-    execute(arg_list);
-    freeTokens(arg_list, c);
-}
-
-void capture()
-{
-    printf("\nmBash-%i> ", getpid());
-    fgets(cmd, MAXLI, stdin);
-    cmd[strlen(cmd) - 1] = '\0';
-}
-
-int nbArguments()
-{
-    int count = 0; //number of arguments
-    for (int loop = 0 ; loop < strlen(cmd) ; ++ loop)
-    {
-        if (cmd[loop] == ' ') ++ count; //counting the number of spaces
-    }
-    return count;
-}
-
-void identifyTokens(char *arg_list[], char *c, int count)
-{
-    char *tmp = strtok(c, " "); //splits the duplicated string using spaces
-    int increment = 0; //index used to browse the arg_list array
-
-    while (tmp != NULL)
-    {
-        char *c = strstr(tmp, "*");
-        if (c == NULL)
-        {
-            arg_list[increment] = strdup(tmp);
-            increment ++;
-        }
-        else
-        {
-            glob_t g;
-            int resGlob = glob(tmp, 0, NULL, &g);
-            if (resGlob == 0)
-            {
-                for (int loop = 0 ; loop < g.gl_pathc ; ++ loop)
-                {
-                    arg_list[increment] = strdup(g.gl_pathv[loop]);
-                    increment ++;
-                }
-            }
-            globfree(&g);
-        }
-        tmp = strtok(NULL, " ");
-    }
-    arg_list[increment] = NULL;
-}
-
 void executeCmd(char *arg_list[])
 {
     pid_t process = fork();
     if (process == 0) //parent process
     {
         int res = execvp(arg_list[0],arg_list);
-        if (res == -1) printf("%s\n",strerror(errno));
+        if (res == -1) printf("%s\n", strerror(errno));
         exit(0);
     }
     else //child process
     {
-        wait(&process);
+        char *test = "&";
+        printf("%d\n", nbArgs);
+        for (int loop = 0 ; loop < nbArgs + 1 ; ++ loop)
+        {
+            printf("%s\t", arg_list[loop]);
+        }
+
+        if ( nbArgs > 0 && strcmp(arg_list[nbArgs], "&") == 0 )
+            sleep(0.5);
+        else
+            wait(&process);
+
     }
 }
 
 void freeTokens(char *arg_list[], char *c)
 {
-    int increment=0;
-    while (arg_list[increment] != NULL)
+    nbArgs = 0;
+    int index = 0;
+    while (arg_list[index] != NULL)
     {
-        free(arg_list[increment]);
-        increment ++;
+        free(arg_list[index]);
+        index ++;
     }
     free(c);
 }
 
 
 /*
-  Builtin functions implementation.
+  Inbuilt functions implementation.
 */
+int inbuiltLength() {
+  return sizeof(inbuiltStr) / sizeof(char *);
+}
+
 int cdF(char *args[])
 {
   if (args[1] == NULL) {
